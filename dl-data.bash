d617df15e9e3aca789b70874cb954d4162883e22
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2018-04-28 13:30:52 +0200

    Dl data.

---
+++ dl-data.bash
@@ -0,0 +1,7 @@
+wget ftp://ftp.ensembl.org/pub/release-92/fasta/homo_sapiens/dna/README --directory-prefix=data/fasta/
+
+wget ftp://ftp.ensembl.org/pub/release-92/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.*.fa.gz --directory-prefix=data/fasta/
+
+wget ftp://ftp.ensembl.org/pub/grch37/release-92/gff3/homo_sapiens/Homo_sapiens.GRCh37.87.chromosome.*.gff3.gz --directory-prefix=data/annotations/
+
+
