bde91fce96e235432f8bca3c38e0912db06d1f43
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2018-04-28 19:22:19 +0200

    Chance varname

--- main.c
+++ main.c
@@ -84,7 +84,7 @@ int build_genome(){
   int c; // getc will send a char as an int
   int num_base = 0; // number of DNA base that have been parsed
 
-  char genome_chunk[12 + 1] = "            \0"; // the chunk that will fill a BD struct
+  char DNAsequence_chunk[12 + 1] = "            \0"; // the chunk that will fill a BD struct
   // +1 for the string end
 
   while((c = getc(fasta_file)) != -1) {
@@ -101,12 +101,12 @@ int build_genome(){
       // base character A, T, C, or G
 
       if (num_base%12 == 0){
-        printf("genome_chunk if full : %s. chunk num : %i\n", genome_chunk, num_base/12);
+        printf("DNAsequence_chunk if full : %s. chunk num : %i\n", DNAsequence_chunk, num_base/12);
       }
 
       printf("to keep : %c\n", c);
       printf("%i mod %i = %d \n", num_base, 12, num_base % 12 );
-      genome_chunk[num_base % 12]=c;
+      DNAsequence_chunk[num_base % 12]=c;
       num_base+=1; // this is a correct base
 
     }else{
